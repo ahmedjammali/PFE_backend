094cc9d177b9ffcf38142ea5846f837a838b14d8
ahmedjammali <ahmedjammali2002@gmail.com>
2024-04-01 22:06:42 +0100

    modify the team api

--- apis/team.py
+++ apis/team.py
@@ -10,15 +10,17 @@ cursor = conn.cursor()
 
 @team_blueprint.route('/check_team_leader', methods=['POST'])
 def check_team_leader():
-    user_id = request.json.get('user_id')
+    user_role = request.json.get('user_role')
+    team_id =request.json.get('team_id')
 
-    if user_id is None:
+    if user_role is None or team_id is None:
         return jsonify({'error': 'User ID not provided'}), 400
 
-    cursor.execute("SELECT * FROM TriSQR_Team WHERE Team_Leader = ?", (user_id,))
-    team_leader = cursor.fetchone()
+    
 
-    if team_leader:
+    if user_role == 3:
+        cursor.execute("SELECT * FROM TriSQR_Team WHERE Id = ?", (team_id,))
+        team_leader = cursor.fetchone()
         team_id = team_leader[0]
         team_name = team_leader[1]
         cursor.execute("SELECT Users_name , Users_Last_Name , Users_Login , privilege_id FROM TriSQR_Users WHERE Team = ? and IsActive = 1", (team_id,))
